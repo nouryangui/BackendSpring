20da28d7c7e002272b146fc281c964cea6794e87
nouryangui <nourelimen.yangui@gmail.com>
2020-10-18 13:36:18 +0200

    add microservice membre

---
+++ membre/src/main/java/tn/enis/membre/MembreApplication.java
@@ -0,0 +1,13 @@
+package tn.enis.membre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MembreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MembreApplication.class, args);
+	}
+
+}
